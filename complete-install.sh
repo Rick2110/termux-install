0cf9299e73f0e6121a638e730b5ff3bfdde2ad07
Henrique <96449635+Kalli211@users.noreply.github.com>
2024-06-05 19:58:19 -0300

    Update complete-install.sh

--- complete-install.sh
+++ complete-install.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
 termux-setup-storage
-pkg update
-pkg update -y
-apt update
-apt upgrade
-pkg install python
-pkg install git
-pkg install nano
-pkg install wget
-pkg install vim
-pkg install curl
-pkg install nodejs
-pkg install openssh
-pkg install gcc
-pkg install imagemagick
-pkg install ffmpeg
-pkg install sqlite
-pkg install python-dev
+pkg update -y && pkg upgrade -y && apt update -y && apt upgrade -y && pkg install python && pkg install git && pkg install nano && pkg install wget && pkg install vim && pkg install curl
+&& pkg install openssh && pkg install imagemagick && pkg install ffmpeg && pkg install sqlite && pkg install python-dev
